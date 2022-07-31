e0c122213111fdabcc475efc56eb2cd88ae7b75f
VasiletsFour <vasilets.seven@gmail.com>
2022-07-31 18:55:30 +0300

    Change url to static

--- src/shared/request/gumtree.req.ts
+++ src/shared/request/gumtree.req.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {ApiResponseData, PostParserSendBody, PostParserSendReplay} from "../../modules/request/request.type";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: "https://gumtree-parser-api.herokuapp.com",
     timeout: 50000,
 })
 
